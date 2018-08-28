a8b624a42fa586f1329c77a700400dba01013bbe
Mórász Bálint <morasz.balint@encosoft.hu>
2018-08-28 15:59:18 +0200

    Add project files.

---
+++ EfCore_Bug_HasDataWithHasComputedColumnSql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCore_Bug_HasDataWithHasComputedColumnSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
